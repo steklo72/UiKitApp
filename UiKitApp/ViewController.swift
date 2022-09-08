843ba4b2aeaa0ba9694f840eb017210a024ae6ea
Aleksandr Fedotov <sasha4936@yandex.ru>
2022-09-08 11:50:45 +0500

    Initial Commit

---
+++ UiKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiKitApp
+//
+//  Created by Fedotov Aleksandr on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
